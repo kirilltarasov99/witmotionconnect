d66e416d1fe6a1ae0dfea938c662d6d03b5c5c5f
Kirill Tarasov <k.tarasov@g.nsu.ru>
2024-04-05 16:19:03 +0700

    change recorder parameters for win nt

--- WitMotionConnect.py
+++ WitMotionConnect.py
@@ -48,8 +48,12 @@ class WitMotionConnect(object):
 
         if not self.vcap_params_path.is_file():
             self._view.output_textEdit.append('Создание дефолт параметров для рекордера')
-            lines = ['use\n', '1\n', 'address\n', '/dev/video2\n', 'res\n', '1920x1080\n', 'fps\n', '60\n',
-                     'livefeed\n', '1280x720\n']
+            if os.name == 'nt':
+                lines = ['use\n', '1\n', 'address\n', '0\n', 'res\n', '1920x1080\n', 'fps\n', '60\n',
+                         'livefeed\n', '1280x720\n']
+            else:
+                lines = ['use\n', '0\n', 'address\n', '/dev/video2\n', 'res\n', '1920x1080\n', 'fps\n', '60\n',
+                         'livefeed\n', '1280x720\n']
             with open(self.vcap_params_path, 'w') as file:
                 file.writelines(lines)
 
