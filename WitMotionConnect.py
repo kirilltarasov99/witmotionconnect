8da5bb5ce5c9c3c9736e17f6b3e222722d6906a3
Kirill Tarasov <k.tarasov@g.nsu.ru>
2024-09-24 16:07:04 +0700

    qimage format fix

--- WitMotionConnect.py
+++ WitMotionConnect.py
@@ -349,7 +349,7 @@ class CameraVideoFeed(QWidget):
         rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
         h, w, ch = rgb_image.shape
         bytes_per_line = ch * w
-        convert_to_Qt_format = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format_RGB888)
+        convert_to_Qt_format = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format.Format_RGB888)
         p = convert_to_Qt_format.scaled(self.display_width, self.display_height, Qt.AspectRatioMode.KeepAspectRatio)
         return QPixmap.fromImage(p)
 
